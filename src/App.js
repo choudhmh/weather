3aeca6a7378668ce293e0e6b1791346b75185045
Mahmud Choudhury <mahmud_uk@hotmail.com>
2023-04-12 05:46:51 +0100

    App.js updated

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import logo from "./logo.svg";
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+// import weatherApp from './components/weatherApp';
+
+import { useState } from "react";
+
+function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=08acd577f8ab8cabf637f7cd3736a629`
+
+  const searchLocation = (event) =>{
+    if(event.key ==='Enter'){
+      axios.get(url).then((response) =>{
+      setData(response.data)
+      console.log(response.data)
+      console.log(data.weather)
+    })
+    setLocation('')
+    }
+    
+  }
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder="Enter Location"
+        type="text"/>
+
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p> {data.name} </p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{Math.round(data.main.temp)} Celsius</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+      
+      {data.name !=undefined &&
+       <div className="bottom">
+          <div className="feels">
+          {data.weather ? <p>{Math.round(data.main.feels_like)}</p> : null}
+
+            <p> Feels like</p>
+          </div>
+          <div className="humdity">
+          {data.weather ? <p>{data.main.humidity}</p> : null}
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+          {data.weather ? <p>{data.wind.speed}</p> : null}
+            <p> Wind Speed</p>
+          </div>
+        </div>
+      }
+       
+      </div>
+    </div>
+  );
+}
+
+export default App;
