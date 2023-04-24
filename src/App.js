1ceef770948be391dc56c343f46b86cbcd61efac
Mahmud Choudhury <mahmud_uk@hotmail.com>
2023-04-24 17:03:39 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,108 +1,66 @@
-import React, { useEffect } from "react";
-import axios from "axios";
 import "./App.css";
+import DisplayDifferentLocation from "./component/displayDifferentLocation";
+import SearchCities from "./component/searchCities";
+import { Route, Routes, NavLink , BrowserRouter, } from "react-router-dom";
+import Test from "./component/test";
+import Dropdown from "./component/DropDown";
+import WeekForecast from "./component/weekForecast";
 
-import { useState } from "react";
-
-function App({ searchCities }) {
-  const [city, setCity] = useState(null);
-  const [weatherData, setWeatherData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const [selected, setSelected] = useState("");
-
-  const getWeatherData = (selectedCity) => {
-    if (selectedCity) {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&units=metric&appid=08acd577f8ab8cabf637f7cd3736a629`;
-      axios.get(url).then((response) => {
-        setWeatherData(response.data);
-        console.log(response.data);
-        setIsLoading(false);
-      });
-    }
-  };
-
-  useEffect(() => {}, [searchCities]);
-
-  const handleChange = (event) => {
-    console.log(event.target.value);
-    setCity(event.target.value);
-
-    setValue(event.target.value);
-  };
-  useEffect(() => {
-    if (city) {
-      setIsLoading(true);
-      getWeatherData(city);
-    }
-  }, [city]);
-
-  const handleSubmit = (event) => {};
-
-  const changeSelectOptionHandler = (event) => {
-    setSelected(event.target.value);
-  };
-
-  const UK = ["Choose City", "London", "Manchester", "Birmingham", "Glasgow"];
-  const France = ["Choose City", "Paris", "Calais", "Lyon", "Nice"];
-  const Spain = ["Choose City", "Barcelona", "Madrid", "Córdoba", "Seville "];
-
-  /** Type variable to store different array for different dropdown */
-  let type = null;
-
-  /** This will be used to create set of options that user will see */
-  let options = null;
-
-  if (selected === "UK") {
-    type = UK;
-  } else if (selected === "France") {
-    type = France;
-  } else if (selected === "Spain") {
-    type = Spain;
-  }
-
-  if (type) {
-    options = type.map((el) => <option key={el}>{el}</option>);
-  }
-
-  const getInitialState = () => {
-    const value = "No City";
-    return value;
-  };
-
-  const [value, setValue] = useState(getInitialState);
-
-  if (isLoading) {
-    return <h2>Still Loading be patient</h2>;
-  }
+function App() {
+ 
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <div>
-          <ul>
-            <p>Europe</p>
-            <select onChange={changeSelectOptionHandler}>
-              <option>Choose Country</option>
-              <option>UK</option>
-              <option>France</option>
-              <option>Spain</option>
-            </select>
-            <p>
-              <select name="city" value={value} onChange={handleChange}>
-                {
-                  /** This is where we have used our options variable */
-                  options
-                }
-              </select>
-            </p>
-            <h3 style={{ color: "red" }}>You have chosen:{` ${value}`}</h3>
-            <h3>Humidity: {weatherData?.main.humidity}</h3>
-            <h3>Temp: {weatherData?.main.temp}</h3>
-          </ul>
+    <>
+    <BrowserRouter>
+        <div style={{
+            display: "flex",
+            background: 'black',
+            padding: '5px 0 5px 5px',
+            fontSize: '20px',
+            justifyContent: "center"
+        }}>
+
+            <div style={{ margin: '10px' }}>
+                <NavLink to="/App
+                " style={({ isActive }) => ({ 
+                    color: isActive ? 'greenyellow' : 'white' })}>
+                    Home
+                </NavLink>
+            </div>
+
+            <div style={{ margin: '10px' }}>
+                <NavLink to="/displayDifferentLocation" style={({ isActive }) => ({ 
+                    color: isActive ? 'greenyellow' : 'white' })}>
+                    Selective Location
+                </NavLink>
+            </div>
+            <div style={{ margin: '10px' }}>
+                <NavLink to="/SearchCities" style={({ isActive }) => ({ 
+                    color: isActive ? 'greenyellow' : 'white' })}>
+                    Specific City
+                </NavLink>
+            </div>
+            <div style={{ margin: '10px' }}>
+                <NavLink to="/weekForecast" style={({ isActive }) => ({ 
+                    color: isActive ? 'greenyellow' : 'white' })}>
+                    Week Forecast
+                </NavLink>
+            </div>
+            
         </div>
-      </div>
-    </form>
+        
+        <Routes>
+           
+            <Route exact path="/displayDifferentLocation" element={<DisplayDifferentLocation />} />
+            <Route exact path="/SearchCities" element={<SearchCities />} />
+            <Route exact path="/weekForecast" element={<WeekForecast />} />
+           
+          
+        </Routes>
+    </BrowserRouter>
+   
+</>
+
   );
 }
 
